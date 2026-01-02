6f3daf201306bd4f10d318fd7593e50cb6497e4e
theaarch <theaarch64@gmail.com>
2026-01-02 23:28:45 +0800

    feat: add conditional JSON exception rendering for webhook and API requests

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -24,5 +25,12 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        // Rendering Exceptions as JSON
+        $exceptions->shouldRenderJsonWhen(function (Request $request, Throwable $e) {
+            if ($request->is('sms-forwarder/webhook')) {
+                return true;
+            }
+
+            return $request->expectsJson();
+        });
     })->create();
